3370d48d231791e9bab28d93c54b06676d7f4dfb
av.karnakov <av.karnakov@ubil-asu1.topprom.local>
2017-04-21 16:06:03 +0700

    Update

--- Swing1/src/ru/lksi/Swing1/First.java
+++ Swing1/src/ru/lksi/Swing1/First.java
@@ -1,10 +1,21 @@
 package ru.lksi.Swing1;
 
+import java.awt.image.BufferedImage;
+
 public class First {
+	static BufferedImage image;
 
 	public static void main(String[] args) {
 		Window w1 = new Window ();
 
+	}
+	
+	static BufferedImage getImage() {
+		return image;
+	}
+
+	static void setImage(BufferedImage image) {
+		First.image = image;
 	}
 
 }
--- Swing1/src/ru/lksi/Swing1/Window.java
+++ Swing1/src/ru/lksi/Swing1/Window.java
@@ -3,27 +3,25 @@ package ru.lksi.Swing1;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import java.awt.Color;
-import java.awt.Component;
-
 import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
-import javax.swing.JTextField;
 import java.awt.Font;
-import javax.swing.JSpinner;
-import java.awt.Choice;
-import javax.swing.SpinnerNumberModel;
+import java.awt.Image;
+
 import javax.swing.JComboBox;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
 import java.awt.event.ActionEvent;
-import javax.swing.JToolBar;
 import javax.swing.JTabbedPane;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 
 public class Window extends JFrame {
@@ -43,10 +41,6 @@ public class Window extends JFrame {
 		tabbedPane.addTab("Tab1", panel1);
 		panel1.setLayout(null);
 		
-		JButton btnGetUrlImage = new JButton("Get URL Image");
-		btnGetUrlImage.setBounds(10, 11, 105, 23);
-		panel1.add(btnGetUrlImage);
-		
 		JButton btnGetFile = new JButton("Get File");
 		btnGetFile.setBounds(660, 11, 89, 23);
 		panel1.add(btnGetFile);
@@ -59,13 +53,35 @@ public class Window extends JFrame {
 		tabbedPane.addTab("Tab2", panel2);
 		panel2.setLayout(null);
 		
-		JButton btnView = new JButton("View");
-		btnView.setBounds(10, 11, 89, 23);
-		panel2.add(btnView);
+		JButton btnGetUrlImage = new JButton("Get URL Image");
+		btnGetUrlImage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					URL url = new URL (txtrEnterImageUrl.getText());
+					First.setImage(ImageIO.read(url));
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(null, "Invalid URL or internet connection failed");
+					//e.printStackTrace();
+				}
+			}
+		});
+		btnGetUrlImage.setBounds(10, 11, 122, 23);
+		panel1.add(btnGetUrlImage);
 		
 		JLabel lblFormat = new JLabel("Format:");
 		lblFormat.setBounds(611, 15, 46, 14);
 		panel2.add(lblFormat);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 40, 739, 432);
+		panel2.add(scrollPane);
+		
+		JPanel panel = new JPanel();
+		scrollPane.setViewportView(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblImagelabel = new JLabel("");
+		panel.add(lblImagelabel);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(667, 12, 82, 20);
@@ -73,16 +89,37 @@ public class Window extends JFrame {
 		comboBox.addItem("jpeg");
 		panel2.add(comboBox);
 		
+		JButton btnView = new JButton("View");
+		btnView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (First.getImage() == null) {return;}
+				lblImagelabel.setIcon(new ImageIcon(First.getImage()));
+				lblImagelabel.updateUI();
+			}
+		});
+		
+				
+		btnView.setBounds(10, 11, 89, 23);
+		panel2.add(btnView);
+		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
+		JMenuItem mntmSaveImage = new JMenuItem("Save Image");
+		mnFile.add(mntmSaveImage);
+		
 		JMenu mnOptions = new JMenu("Options");
 		menuBar.add(mnOptions);
 		
 		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
 		mnOptions.add(mntmExit);
 		
 		
